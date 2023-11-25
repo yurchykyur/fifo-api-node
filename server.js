f6f1af0cb9b586eefcbf828d05aecb3d8655fedd
yurchykyur <yurchyk.yur.s@gmail.com>
2023-11-25 15:45:30 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require("./app");
+
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+const { MONGODB_CONNECTION_STRING } = process.env;
+
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log("Database connection successful");
+
+    app.listen(PORT, () =>
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
